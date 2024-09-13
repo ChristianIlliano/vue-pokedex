9e9b4ef33e9f0b90598512676308a1a1776283ff
ChristianIlliano <grafica.illiano@gmail.com>
2024-09-13 15:07:09 +0200

    feat: API call

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import axios from "axios";
+import { store } from './store';
+import SearchBar from './components/SearchBar.vue';
+export default {
+  data() {
+    return {
+    store,
+    }
+  },
+  components: {
+    SearchBar,
+  },
+
+  created() {
+
+  },
+
+  methods: {
+    getPokemon() {
+      const searchQuery = this.store.searchQuery;
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${searchQuery}`, {
+        params: {
+          searchQuery: this.store.searchQuery,
+          
+        }
+    }).then((resp) => {
+      console.log(resp);
+      
+    })
+    } 
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="pokedex">
+    <SearchBar @poke-search="getPokemon"/>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<script>
+import { store } from '../store';
+export default {
+
+    emits: ["pokeSearch"],
+
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+<template>
+    <!-- SearchBar -->
+    <label for="">
+        <input type="text" v-model="store.searchQuery">
+    </label>
+    <button class="btn-search" @click="$emit('pokeSearch')">Cerca</button>
+
+</template>
+
+<style></style>
